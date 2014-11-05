06aa247a3526cf1d7d34d2b2a9d7a57074e6a9e7
KRCPRUEBAS <KRCPRUEBAS@172.16.17.214>
2014-11-05 15:40:53 -0500

    Servidor funcionado Correctamente

--- src/com/kradac/karview/netty/pruebas.java
+++ src/com/kradac/karview/netty/pruebas.java
@@ -11,12 +11,12 @@ package com.kradac.karview.netty;
  */
 public class pruebas {
 
-    public static void main(String[] args) {
-        String datos = "0@80          9     CM10     459  14 GPRMC,215634.00,A,0326.65002,S,07957.58693,W,0.000,0.0,291014,,,A*52    55446007";
-        String[] dataTrama = datos.split(",");
-        String gpiodat = dataTrama[1].trim();
-        String gpiodata[] = gpiodat.split(" ");
-//        System.out.println(gpiodata[1]);
-////        System.out.println(Short.parseShort(dataHeader[1].substring(0, 1)));
-    }
+//    public static void main(String[] args) {
+////        String datos = "0@80          9     CM10     459  14 GPRMC,215634.00,A,0326.65002,S,07957.58693,W,0.000,0.0,291014,,,A*52    55446007";
+////        String[] dataTrama = datos.split(",");
+////        String gpiodat = dataTrama[1].trim();
+////        String gpiodata[] = gpiodat.split(" ");
+//////        System.out.println(gpiodata[1]);
+////////        System.out.println(Short.parseShort(dataHeader[1].substring(0, 1)));
+//    }
 }
